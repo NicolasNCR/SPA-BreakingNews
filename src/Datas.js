9bd35b30c81cafed3a16361f7d7bd5b0cdaee874
Nicolas Costa Ribeiro <nribeiro.projetos@gmail.com>
2024-04-15 22:06:15 -0300

    feat: create data mock

---
+++ src/Datas.js
@@ -0,0 +1,23 @@
+export const news = [
+    {
+        title: "Notícia 1",
+        text: "Lorem10Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a felis et diam sagittis elementum vitae in justo. Donec pellentesque consectetur orci id cursus. Morbi at condimentum metus. Suspendisse fermentum porttitor ornare. Proin suscipit tincidunt nibh, sed aliquam mauris pulvinar et. Nunc vitae est sit amet nisi lacinia ultricies.",
+        image: "https://cdn.icon-icons.com/icons2/510/PNG/512/images_icon-icons.com_50365.png",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "Notícia 2",
+        text: "Lorem10Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a felis et diam sagittis elementum vitae in justo. Donec pellentesque consectetur orci id cursus. Morbi at condimentum metus. Suspendisse fermentum porttitor ornare. Proin suscipit tincidunt nibh, sed aliquam mauris pulvinar et. Nunc vitae est sit amet nisi lacinia ultricies.",
+        image: "https://cdn.icon-icons.com/icons2/510/PNG/512/images_icon-icons.com_50365.png",
+        likes: 2,
+        comments: 4
+    },
+    {
+        title: "Notícia 3",
+        text: "Lorem10Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam a felis et diam sagittis elementum vitae in justo. Donec pellentesque consectetur orci id cursus. Morbi at condimentum metus. Suspendisse fermentum porttitor ornare. Proin suscipit tincidunt nibh, sed aliquam mauris pulvinar et. Nunc vitae est sit amet nisi lacinia ultricies.",
+        image: "https://cdn.icon-icons.com/icons2/510/PNG/512/images_icon-icons.com_50365.png",
+        likes: 2,
+        comments: 4
+    }
+]
