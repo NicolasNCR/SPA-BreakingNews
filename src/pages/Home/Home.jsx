04cd6995757e1e7624aca7a9eaa4c5cb786173cf
Nicolas Costa Ribeiro <nribeiro.projetos@gmail.com>
2024-04-17 22:15:19 -0300

    feat: create navbar component, add bootstrap icons

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,10 @@
+import { Navbar } from "../../components/Navbar/Navbar";
+
 export default function Home(){
-    return <h1>Hello Home!</h1>
+    return (
+        <> {/* Fragment: tag vazia */}
+            <Navbar />
+            <h1>Hello Home!</h1>
+        </>
+    )
 }
